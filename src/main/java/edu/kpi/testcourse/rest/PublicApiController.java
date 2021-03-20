d7634e5bd8a1ca48ade746a74b4cb203f373076b
Narberial <58740649+Narberial@users.noreply.github.com>
2021-03-20 19:18:00 +0200

    Update PublicApiController.java (#8)

--- src/main/java/edu/kpi/testcourse/rest/PublicApiController.java
+++ src/main/java/edu/kpi/testcourse/rest/PublicApiController.java
@@ -2,19 +2,35 @@ package edu.kpi.testcourse.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.kpi.testcourse.entities.UrlAlias;
 import edu.kpi.testcourse.logic.Logic;
 import edu.kpi.testcourse.rest.models.ErrorResponse;
+import edu.kpi.testcourse.rest.models.UrlAllResponse;
+import edu.kpi.testcourse.rest.models.UrlShortenResponse;
 import edu.kpi.testcourse.rest.models.UserSignupRequest;
+import edu.kpi.testcourse.serialization.JsonTool;
+import edu.kpi.testcourse.serialization.JsonToolJacksonImpl;
+import edu.kpi.testcourse.storage.UrlRepository.PermissionDenied;
+import edu.kpi.testcourse.storage.UrlRepositoryFakeImpl;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.server.util.HttpHostResolver;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Inject;
+
 
 /**
  * API controller for all REST API endpoints accessible without authentication.
@@ -25,9 +41,13 @@ public class PublicApiController {
 
   private final Logic logic;
   private final ObjectMapper objectMapper;
+  JsonToolJacksonImpl json = new JsonToolJacksonImpl();
+  private Map<String, Map<String, String>> data = new HashMap<>();
+
 
   @Inject
-  public PublicApiController(Logic logic, ObjectMapper objectMapper) {
+  public PublicApiController(Logic logic, ObjectMapper objectMapper,
+      HttpHostResolver httpHostResolver) {
     this.logic = logic;
     this.objectMapper = objectMapper;
   }
@@ -45,7 +65,7 @@ public class PublicApiController {
       return HttpResponse.status(HttpStatus.CREATED);
     } catch (Logic.UserIsAlreadyCreated e) {
       return HttpResponse.serverError(
-        objectMapper.writeValueAsString(new ErrorResponse(0, e.getMessage())));
+          objectMapper.writeValueAsString(new ErrorResponse(0, e.getMessage())));
     }
   }
 
@@ -55,12 +75,38 @@ public class PublicApiController {
    * @param alias a short URL alias
    */
   @Get(value = "/r/{alias}")
-  public HttpResponse<?> redirect(String alias) {
+  public HttpResponse<?> redirect(String alias) throws URISyntaxException {
     String fullUrl = logic.findFullUrl(alias);
     if (fullUrl != null) {
-      return HttpResponse.redirect(URI.create(fullUrl));
+      return HttpResponse.redirect(new URI(fullUrl));
     } else {
       return HttpResponse.notFound();
     }
   }
+
+
+  /**
+   * Send list as response.
+   *
+   */
+  @Get(value = "/urls")
+  public HttpResponse<String> showUrls(HttpRequest<?> httpRequest, Principal principal) {
+    String email = principal.getName();
+    return HttpResponse.created(json.toJson(logic.dataCreation().get(email)));
+  }
+
+
+  /**
+   * Delete full URL by alias.
+   *
+   * @param alias a short URL alias
+   */
+  @Delete(value = "/delete/{alias}")
+  public HttpResponse<?> deleteAlias(String alias, Principal principal) throws PermissionDenied {
+
+    String email = principal.getName();
+
+    return logic.deleteFunc(email, alias);
+  }
+
 }
